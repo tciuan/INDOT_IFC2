c04fedc80c4090c301f436cf1208374a6efdfda5
tciuan <tian154@purdue.edu>
2021-07-11 19:38:20 -0400

    Add project files.

---
+++ INDOT_IFC2/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿
+namespace INDOT_IFC2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Salmon;
+            this.button1.Location = new System.Drawing.Point(291, 114);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(197, 74);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Get_IfcObject_Information";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Salmon;
+            this.button2.Location = new System.Drawing.Point(290, 214);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(198, 73);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Ifc_Info_Checking";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::INDOT_IFC2.Properties.Resources.CARELABLOGO;
+            this.pictureBox1.Location = new System.Drawing.Point(480, 391);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(139, 120);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::INDOT_IFC2.Properties.Resources.AutoIC_Lab_Logo;
+            this.pictureBox2.Location = new System.Drawing.Point(636, 391);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(161, 120);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(480, 351);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 27);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Developed by";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(283, 351);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 27);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Support by";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::INDOT_IFC2.Properties.Resources._1200px_Seal_of_the_Indiana_Department_of_Transportation_svg;
+            this.pictureBox3.Location = new System.Drawing.Point(291, 391);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(161, 120);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 19.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(256, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(249, 38);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "IFC Connector";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(25, 351);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(176, 27);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "About this App";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Arial Unicode MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(25, 391);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(211, 83);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "The App help AEC stakeholders better use IFC data. V1.0";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button4.Location = new System.Drawing.Point(41, 165);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(144, 58);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Select IFC file";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(850, 523);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ INDOT_IFC2/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Xbim.Ifc;
+using Xbim.Ifc4.Interfaces;
+using Microsoft.VisualBasic;
+
+
+namespace INDOT_IFC2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void button1_Click(object sender, EventArgs e) // informaiton extraction
+        {
+            //string fileName = System.AppDomain.CurrentDomain.BaseDirectory + "rac_advanced_sample_project.ifc";
+            string fileName = Text;
+            var model = IfcStore.Open(fileName);
+            //select objects                           
+
+            string str = Interaction.InputBox("Please input what kind object you need", "Ifc inforamtion extraction", "", 50, 50);
+
+            if (str == "slab")
+            {
+                var allSlab = model.Instances.OfType<IIfcSlab>();
+                using (StreamWriter sw = new StreamWriter("Ifcslab.txt"))
+                {
+                    foreach (var s in allSlab)
+                    {
+                        sw.WriteLine(s);
+
+                    }
+                }
+            }
+
+            if (str == "door")
+            {
+                var allDoors = model.Instances.OfType<IIfcDoor>();
+                using (StreamWriter sw = new StreamWriter("Ifcdoor.txt"))
+                {
+                    foreach (var s in allDoors)
+                    {
+                        sw.WriteLine(s);
+
+                    }
+                }
+            }
+
+            if (str == "window")
+            {
+                var allWindows = model.Instances.OfType<IIfcWindow>();
+                using (StreamWriter sw = new StreamWriter("Ifcwindow.txt"))
+                {
+                    foreach (var s in allWindows)
+                    {
+                        sw.WriteLine(s);
+                    }
+                }
+            }
+            
+            //Console.WriteLine($"Door ID: {theDoor.GlobalId}, Name: {theDoor.Name}");         
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e) //look for specific object
+        {            
+            //string fileName = System.AppDomain.CurrentDomain.BaseDirectory + "rac_advanced_sample_project.ifc";
+            string fileName = Text;
+            var model = IfcStore.Open(fileName);
+            //var id = "3FZFp0nq9AAhRAecLpGs5a";
+            var id= Interaction.InputBox("Please input Global ID:", "Ifc_Info_Checking", "", 50, 50);
+            var theDoor = model.Instances.FirstOrDefault<IIfcDoor>(d => d.GlobalId == id);
+
+                      
+            
+            var properties = theDoor.IsDefinedBy
+                .Where(r => r.RelatingPropertyDefinition is IIfcPropertySet)
+                .SelectMany(r => ((IIfcPropertySet)r.RelatingPropertyDefinition).HasProperties)
+                .OfType<IIfcPropertySingleValue>();
+            
+            using (StreamWriter sw = new StreamWriter("Ifcobject.txt"))
+                foreach (var property in properties)
+            { 
+                sw.WriteLine($"Property: {property.Name}, Value: {property.NominalValue}");
+
+            }
+
+            
+
+            
+        }
+
+       
+
+        private void button3_Click(object sender, EventArgs e)// get inspection results
+        {
+            string fileName = System.AppDomain.CurrentDomain.BaseDirectory + "ifcbridge-model01_Rev.ifc";
+            var model = IfcStore.Open(fileName);
+            var allBeam = model.Instances.OfType<IIfcBeam>();
+            foreach (var s in allBeam)
+            {
+                //s.Description;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e) // carelab picture box
+        {
+            Help.ShowHelp(this, "https://polytechnic.purdue.edu/facilities/construction-automation-robotics-and-ergonomics-care-lab");
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e) // autoIC picture box
+        {
+            Help.ShowHelp(this, "https://polytechnic.purdue.edu/autoic-lab");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            
+            Help.ShowHelp(this, "https://www.in.gov/indot");
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e) // select input file
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.ShowDialog();
+            this.Text = file.SafeFileName;
+        }
+
+       
+    }
+}
